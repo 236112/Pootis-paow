6b81bf11f824c21d98f08e15a82560d03173fe9f
236112 <60393877+236112@users.noreply.github.com>
2020-02-13 09:20:20 -0600

    Add files via upload

---
+++ Assets/Scripts/Boom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Boom : MonoBehaviour
+{
+
+    private Animator animator; //this creates a variable called animator
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>(); //this assigns the gameObjects Animator to the variable animator
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Debug.Log ("Boom", gameObject);
+        animator.SetBool("Collision", true); //this changes the bool parameter to true and triggers the "Boom" animation
+        Invoke("ChangeLevel", .5f);
+
+    }
+
+    void ChangeLevel()
+    {
+        SceneManager.LoadScene("SampleScene"); //this restarts the current level
+    }
+
+}
